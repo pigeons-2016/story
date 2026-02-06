ccf9cdd321ff32845ab79b2499bbc8262510c479
bakar-git <bakar.git@gmail.com>
2026-02-06 21:30:57 +0500

    feat: little fixes

--- src/App.tsx
+++ src/App.tsx
@@ -37,13 +37,6 @@ export default function App() {
                 Story Behind Countdown
               </button>
             </div>
-
-            {/* Decorative elements */}
-            <div className="mt-12 sm:mt-20 flex justify-center gap-2">
-              <div className="w-2 h-2 rounded-full bg-primary/30"></div>
-              <div className="w-2 h-2 rounded-full bg-primary/50"></div>
-              <div className="w-2 h-2 rounded-full bg-primary/30"></div>
-            </div>
           </>
         )}
       </div>
